8514c8b8a6848cca214498026ee6d5825858c21f
MahendraSH <maahi2001sh@gmail.com>
2024-01-06 15:22:52 +0530

    url axios update

--- lib/config/site-config.ts
+++ lib/config/site-config.ts
@@ -2,6 +2,6 @@ export const siteConfig = {
   name: "Notes-md",
   description:
     "Portfolio Site of Mahendra, a FullStack Developer showcasing his projects and experiences",
-  url: "https://notes-md.vercel.app",
-  ogImage: "https://notes-md.vercel.app/og.png",
+  url: "https://notes-mds.vercel.app",
+  ogImage: "https://notes-mds.vercel.app/og.png",
 };
