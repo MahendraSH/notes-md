624c3f05620ae03ec0a91089250c31c8e899fe28
MahendraSH <maahi2001sh@gmail.com>
2024-01-18 13:18:26 +0530

    spm

--- lib/config/site-config.ts
+++ lib/config/site-config.ts
@@ -2,6 +2,6 @@ export const siteConfig = {
   name: "Notes-md",
   description:
     "Portfolio Site of Mahendra, a FullStack Developer showcasing his projects and experiences",
-  url: "https://notes-mds.vercel.app/",
+  url: "http://localhost:3000/",
   ogImage: "https://notes-mds.vercel.app/og.png",
 };
