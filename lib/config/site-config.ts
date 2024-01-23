034fa33a6a31cf67bc158e40cf95f2334e2eb6ab
MahendraSH <maahi2001sh@gmail.com>
2024-01-23 18:46:25 +0530

    site config

--- lib/config/site-config.ts
+++ lib/config/site-config.ts
@@ -1,8 +1,8 @@
 export const siteConfig = {
   name: "Notes-md",
   description: "notes md ",
-  // url: "https://notes-mds.vercel.app/",
-  url: "http://localhost:3000/",
+  url: "https://notes-mds.vercel.app/",
+  // url: "http://localhost:3000/",
 
   ogImage: "https://notes-mds.vercel.app/og.png",
 };
