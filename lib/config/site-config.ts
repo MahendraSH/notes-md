c7aede84b40e706c022a6b83037ab30250b1b6b5
MahendraSH <maahi2001sh@gmail.com>
2024-01-23 09:58:28 +0530

    og images url changed

--- lib/config/site-config.ts
+++ lib/config/site-config.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   name: "Notes-md",
   description: "notes md ",
-  url: "https://notes-mds.vercel.app/",
+  url: "http://localhost:3000/",
   ogImage: "https://notes-mds.vercel.app/og.png",
 };
