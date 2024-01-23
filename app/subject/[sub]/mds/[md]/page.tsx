770b42b5c6700b094a03f75538553a5380ea5a17
MahendraSH <maahi2001sh@gmail.com>
2024-01-23 18:38:48 +0530

    code hilighting and github link

--- app/subject/[sub]/mds/[md]/page.tsx
+++ app/subject/[sub]/mds/[md]/page.tsx
@@ -4,7 +4,9 @@ import ReactHtmlParser from "react-html-parser";
 import md from "markdown-it";
 import namedHeadings from "markdown-it-named-headings";
 import { siteConfig } from "@/lib/config/site-config";
-
+import markdownItGithubHeadings from "markdown-it-github-headings";
+import highlightjs from "markdown-it-highlightjs";
+import "highlight.js/styles/github.css";
 interface MdPageProps {
   params: {
     sub: string;
@@ -28,12 +30,22 @@ const MdPage: FC<MdPageProps> = async ({ params }) => {
 
   const mddata = await ReadMd();
   const data = md()
-    .set({ linkify: true, typographer: true, breaks: false })
+    .set({ typographer: true, breaks: false })
     .use(namedHeadings)
+    .use(highlightjs, {
+      auto: true,
+      code: true,
+      inline: true,
+    })
+    .use(markdownItGithubHeadings, {
+      prefixHeadingIds: false,
+      enableHeadingLinkIcons: false,
+      prefix: false,
+    })
     .render(mddata);
 
   return (
-    <div className=" sm:w-[cal(100%-20px)] lg: w-full  p-2     prose   *:first-letter:capitalize  prose-headings:text-accent-foreground/75 prose-strong:text-secondary-foreground  prose-strong:text-xl prose-strong:font-semibold  mx-auto mt-8 *:text-foreground prose-code:text-base prose-code:text-muted-foreground prose-code:bg-muted">
+    <div className="  sm:w-[cal(100%-20px)] lg: w-full  p-2     prose   prose-headings:first-letter:capitalize  prose-headings:text-accent-foreground/75 prose-strong:text-secondary-foreground  prose-strong:text-xl prose-strong:font-semibold  mx-auto mt-8 *:text-foreground prose-code:text-base prose-code:text-muted-foreground prose-code:!bg-muted  prose-pre:bg-muted dark:prose-code:!bg-secondary-foreground  prose-pre:p-3  dark:prose-pre:bg-secondary-foreground   ">
       {ReactHtmlParser(data)}
     </div>
   );
