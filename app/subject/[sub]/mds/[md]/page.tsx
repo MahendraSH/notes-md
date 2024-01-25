f1780135ea61ea157206e6518a90fae6980e5dec
MahendraSH <maahi2001sh@gmail.com>
2024-01-25 15:48:13 +0530

    remove : devops

--- app/subject/[sub]/mds/[md]/page.tsx
+++ app/subject/[sub]/mds/[md]/page.tsx
@@ -44,7 +44,7 @@ const MdPage: FC<MdPageProps> = async ({ params }) => {
     .render(mddata);
 
   return (
-    <div className="  sm:w-[cal(100%-20px)] lg: w-full  p-2     prose   prose-headings:first-letter:capitalize  prose-headings:text-accent-foreground/75 prose-strong:text-secondary-foreground  prose-strong:text-xl prose-strong:font-semibold  mx-auto mt-8 *:text-foreground prose-code:text-base prose-code:text-muted-foreground prose-code:!bg-muted/70  prose-pre:bg-muted dark:prose-code:!bg-secondary dark:prose-code:!text-secondary-foreground  prose-pre:p-3  dark:prose-pre:bg-secondary   ">
+    <div className="  sm:w-[cal(100%-20px)] lg: w-full  p-2     prose   prose-headings:first-letter:capitalize  prose-headings:text-accent-foreground/75 prose-strong:text-secondary-foreground  prose-strong:text-xl prose-strong:font-semibold  mx-auto mt-8 *:text-foreground prose-code:text-base prose-code:text-muted-foreground prose-code:!bg-muted/70  prose-pre:bg-muted dark:prose-code:!bg-secondary dark:prose-code:!text-secondary-foreground  prose-pre:p-3  dark:prose-pre:bg-secondary  prose-code:data-theme='default' ">
       {ReactHtmlParser(data)}
     </div>
   );
